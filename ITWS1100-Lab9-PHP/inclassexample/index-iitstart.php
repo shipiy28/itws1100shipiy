5d12b74f0a8ab6134c82d358730d1aec6e1acdd1
shipiy28 <shipiy@rpi.edu>
2025-12-09 20:33:36 -0500

    Update index-iitstart.php

--- ITWS1100-Lab9-PHP/inclassexample/index-iitstart.php
+++ ITWS1100-Lab9-PHP/inclassexample/index-iitstart.php
@@ -99,7 +99,7 @@
         $statement->bind_param("sss",$lastNameForDb,$firstNamesForDb,$dobForDb);
         // make it so:
         $statement->execute();
-
+        alert("test");
         // give the user some feedback
         echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' actor added to database.</h4>';
         echo $firstNames . ' ' . $lastName . ', born ' . $dob . '</div>';
